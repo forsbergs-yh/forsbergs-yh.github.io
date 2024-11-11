804ead028a5ccb05c27bf0f8f0a97f0c175a46fd
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-11 16:00:16 +0100

    .

--- scripts/main.js
+++ scripts/main.js
@@ -6,13 +6,19 @@
 3: Your app accesses the API with the user's access token
 */
 const clientId = 'Ov23liGusittSCIzNE2p';  // Client ID of the GitHub OAuth app
-const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&state=${state}`; // Formatted string inside backticks
+const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_url=${redirectURL}&state=${state}`; // Formatted string inside backticks
+const redirectURL = `https://forsbergs-yh.github.io/home`;
 const state = Math.random().toString(36).substring(2);  // Generating a random string for the state parameter
 
 window.onload = myFunction; // Executions of referred function
 
+
 // "myFunction" can be named any other "camelCase" defined name of choice
 function myFunction() // Desired executions of myFunction defined below inside {}:
 {
-    window.location.href = url; // Navigates to 'url'
+    fetch(
+        url, {
+            method: 'GET'
+        }
+    ) ;
 }
