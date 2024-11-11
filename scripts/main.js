9f590d509d9f11475544207b4ad7a2deba737da3
sibjor <sixten.bjorling@student.forsbergsskola.se>
2024-11-11 16:36:35 +0100

    System updated

--- scripts/main.js
+++ scripts/main.js
@@ -6,19 +6,16 @@
 3: Your app accesses the API with the user's access token
 */
 const clientId = 'Ov23liGusittSCIzNE2p';  // Client ID of the GitHub OAuth app
-const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_url=${redirectURL}&state=${state}`; // Formatted string inside backticks
-const redirectURL = `https://forsbergs-yh.github.io/home`;
 const state = Math.random().toString(36).substring(2);  // Generating a random string for the state parameter
+const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&state=${state}`; // Formatted string inside backticks
 
 window.onload = myFunction; // Executions of referred function
 
 
-// "myFunction" can be named any other "camelCase" defined name of choice
+// "myFunction" can be defined with any appropriate name using the "camelCase" naming convention
 function myFunction() // Desired executions of myFunction defined below inside {}:
 {
-    fetch(
-        url, {
-            method: 'GET'
-        }
-    ) ;
+    var iframe = document.createElement('iframe');
+    iframe.src = url;
+    document.body.appendChild(iframe); // Append 'iframe' as a child element of body to the html document
 }
